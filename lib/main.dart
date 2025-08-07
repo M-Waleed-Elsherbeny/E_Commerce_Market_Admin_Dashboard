0eda5b922536eeff8dab6dfa811a72e2bf51119a
Mphammed Waleed <m.elsherbeny100@gmail.com>
2025-08-07 09:34:28 +0300

    Finish Admin Dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
-  // await Supabase.initialize(url: SUPABASE_URL, anonKey: SUPABASE_ANON_KEY);
   Bloc.observer = MyBlocObserver();
   runApp(const MyApp());
 }
@@ -15,7 +14,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
